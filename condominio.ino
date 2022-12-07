95efe0df9e793a1947ea85dda187098482e0c2b6
jackyeneas <58607680+jackyeneas@users.noreply.github.com>
2022-12-07 16:20:07 -0300

    Add files via upload

--- condominio.ino
+++ condominio.ino
@@ -35,10 +35,10 @@ char password[] = "arduino123"; //senha banco
 
 
 //STRING PARA BANCO
-char INSERIR_AGUA[] = "INSERT INTO agua (id_casa, quant_consumo) VALUES (%s, %s)";
-char INSERIR_LUZ[] = "INSERT INTO energia (id_casa, quant_consumo) VALUES (%s, %s)";
-char STATUS_BOTAOAGUA[] = "SELECT (status_agua) FROM casa WHERE id_casa = Casa";
-char STATUS_BOTAOLUZ[] = "SELECT (status_energia) FROM casa WHERE id_casa = Casa";
+char INSERIR_AGUA[] = "INSERT INTO agua (quant_consumo, id_casa) VALUES (%s, 1)"; //inserir numero da casa manual
+char INSERIR_LUZ[] = "INSERT INTO energia (quant_consumo, id_casa) VALUES (%s, 1)"; //inserir numero da casa manual
+char STATUS_BOTAOAGUA[] = "SELECT (status_agua) FROM casa WHERE id_casa = 1"; //inserir numero da casa manual
+char STATUS_BOTAOLUZ[] = "SELECT (status_energia) FROM casa WHERE id_casa = 1"; //inserir numero da casa manual
 char BANCODEDADOS[] = "USE projeto_condominio"; //Escolhe o banco
 
 //Instanciando objetos
@@ -46,24 +46,33 @@ EthernetClient client; //responsável pela comunicação via ethernet
 MySQL_Connection conn((Client *)&client); //será responsável pela conexão com o servidor MySQL 
 
 
-//global
+//global sql
 int leitura; 
 float leituraconvertida;
 char sentenca[128]; //isso vai puro para o banco
+int lerbanco; 
 
-//leitura agua
+//global millis
+unsigned long int tempo_atual = 0;
+unsigned long ultimo_tempo = 0;
+
+
+//leitura agua sql
 char valorAgua[10];
 
-//leitura luz
+//leitura luz sql
 char valorLuz[10];
 
-//~~variaveis leitura botao fisico agua
-//FAZER
-//~~variaveis leitura botao fisico luz
-//FAZER
-//~~variaveis leitura sensor presença
-//FAZER
 
+//Variaveis Globais de leitura de sensores
+volatile byte valorBotaoAgua = LOW;
+volatile byte valorBotaoLuz = LOW;
+volatile byte estadoBotaoAgua = LOW;
+volatile byte estadoBotaoLuz = LOW;
+volatile byte valorPresenca = LOW;
+
+
+//---------------------------------------------------------------------------------------------------------------------
 
 void setup() 
 { 
@@ -99,62 +108,130 @@ void setup()
   //definindo todos os leds por padrão como desligados
    digitalWrite(LedAgua, LOW);
    digitalWrite(LedLuz, LOW);
-   digitalWrite(ReleAgua, HIGH);
-   digitalWrite(ReleLuz, HIGH);
    digitalWrite(Lampada, LOW);  
   
 }
 
+//---------------------------------------------------------------------------------------------------------------------
 
 void loop() 
 {
+  
+  
+   LendoSensor();
+   ControleAgua();
+   ControleLuz();
+   Lampada(); 
    leituraAgua();
    leituraLuz();
+   leituraBanco();
+
+  
+  
+ // 	if(((millis() - controleLeitura) > esperaLeitura))
+ //   	enviaDados(); 60000
+ //   delay(10);
 }
 
+
+void LendoSensor(){
+ estadoBotaoAgua = digitalRead(ButAgua);
+ if (estadoBotaoAgua == HIGH) valorBotaoAgua=!valorBotaoAgua;
+ 
+ estadoBotaoLuz = digitalRead(ButLuz);
+ if (estadoBotaoLuz == HIGH) valorBotaoLuz=!valorBotaoLuz;
+  
+ valorPresenca = digitalRead(SenPres);
+
+}
+
+
+void ControleAgua(){ 
+  if (estadoBotaoAgua == HIGH) digitalWrite(ReleAgua, valorBotaoAgua);
+}    
+
+void ControleLuz(){ 
+  if (estadoBotaoLuz == HIGH) digitalWrite(ReleLuz, valorBotaoLuz);
+}    
+  
+
+void Lampada(){
+    tempo_atual = millis();
+  
+    if (valorPresenca == HIGH){ 
+    ultimo_tempo = tempo_atual;
+    digitalWrite(Lampada, HIGH);  
+    }  
+           
+  	if ((valorPresenca == LOW) && (tempo_atual - ultimo_tempo >= 5000)){         
+    digitalWrite(Lampada, LOW);
+    }
+}
+    
 
 void leituraAgua(){
    Serial.println("Executando leitura");
    leitura = analogRead(SenAgua); //recebe valor do sensor
-   leituraconvertida = (float(analogRead(SenAgua))*5/(1023))/0.1; //converte os valores de sensor para valores reais
+   leituraconvertida = (float(analogRead(SenAgua))/2.28; //converte os valores de sensor para valores reais, 
    Serial.print("Agua: "); //mostra leitura
    Serial.println(round(leituraconvertida)); //mostra leitura
    dtostrf(leituraconvertida, 6, 2, valorAgua); //converte o valor da leitura para uma char para poder enviar ao banco, existem quatro parâmetros, onde estes são: a variável do tipo float que queremos converter, o número de algarismos que o resultado da conversão deve ter (incluindo a vírgula), o número de casas após a vírgula e o vetor de char em que será armazenada a informação retornada por esta função
    sprintf(sentenca, INSERIR_AGUA, valorAgua); //construir a sentença contendo a instrução que será utilizada na manipulação do banco de dados. O primeiro parâmetro desta função consiste na variável do tipo char na qual será armazenada a sentença após ser construída, o segundo, deve conter a variável que armazenou a parte textual da frase e o local onde será inserida a variável (%s) e o terceiro parâmetro é justamente a variável que será inserida no local especificado no segundo parâmetro   
    enviaDados();
-   delay(10);
+  
+   if(leituraconvertida>22,5) digitalWrite(LedAgua, HIGH); 
+   else digitalWrite(LedAgua, LOW);
 }
 
 void leituraLuz(){
    Serial.println("Executando leitura");
    leitura = analogRead(SenLuz); //recebe valor do sensor
-   leituraconvertida = (float(analogRead(SenLuz))*5/(1023))/0.1; //converte os valores de sensor para valores reais
+   leituraconvertida = (float(analogRead(SenLuz))/50; //converte os valores de sensor para valores reais
    Serial.print("Luz: "); //mostra leitura
    Serial.println(round(leituraconvertida)); //mostra leitura
    dtostrf(leituraconvertida, 6, 2, valorLuz); //converte o valor da leitura para uma char para poder enviar ao banco, existem quatro parâmetros, onde estes são: a variável do tipo float que queremos converter, o número de algarismos que o resultado da conversão deve ter (incluindo a vírgula), o número de casas após a vírgula e o vetor de char em que será armazenada a informação retornada por esta função
    sprintf(sentenca, INSERIR_LUZ, valorLuz); //construir a sentença contendo a instrução que será utilizada na manipulação do banco de dados. O primeiro parâmetro desta função consiste na variável do tipo char na qual será armazenada a sentença após ser construída, o segundo, deve conter a variável que armazenou a parte textual da frase e o local onde será inserida a variável (%s) e o terceiro parâmetro é justamente a variável que será inserida no local especificado no segundo parâmetro
    enviaDados();
-   delay(10);
+  
+     if(leituraconvertida>1.02) digitalWrite(LedLuz, HIGH);
+     else digitalWrite(LedLuz, LOW);
 }
 
-
-/*void loop(){
-	if(((millis() - controleLeitura) > esperaLeitura))
-    	enviaDados();
-    delay(10);
-}
-*/
 
 void enviaDados(){	
  	MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn); //prepara para enviar ao banco
    	cur_mem->execute(sentenca); //deste vez é a sentença para incluir uma linha na tabela
     delete cur_mem; //limpa memória do arduíno após enviar ao banco
-    delay(60000); //delay de um minuto
+    delay(1000); //delay de um millis
 }
+
     
-//VER O DELAY
-    
-    
+void leituraBanco(){
+   
+   row_values *row = NULL;
+   //long lerbanco = 0;
+   delay(1000);
+
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  cur_mem->execute(STATUS_BOTAOAGUA);
+  
+  column_names *columns = cur_mem->get_columns();
+
+  do {
+    row = cur_mem->get_next_row();
+    if (row != NULL) {
+      lerbanco = atol(row->values[0]);
+    }
+  } while (row != NULL);
+
+  delete cur_mem;
+
+  Serial.print("  Status Botao = ");
+  Serial.println(lerbanco);
+
+  delay(500); 
+   
+}
     
     
 
